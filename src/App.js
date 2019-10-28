ec6e81de77c1e30665ad794fe0e9ae0639465e40
JChartier001 <JChartier001@gmail.com>
2019-10-28 10:33:18 -0500

    added form and background pic.  Changed position of todolist

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import ToDoList from "./components/TodoComponents/TodoList"
+import "../src/components/TodoComponents/Todo.css";
 
 class App extends React.Component {
+  state = {toDo: ""};
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
   render() {
     return (
-      <div>
-        <h2>Welcome to your Todo App!</h2>
+      <div className="App">
+        <ToDoList />
       </div>
     );
   }
--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,19 @@
-// your components will all go in this `component` directory.
-// feel free to change this component.js into TodoList.js
+import React from "react";
+
+
+const handleButtonClick = () => {
+    
+}
+
+const ToDoList = () =>{
+    return (
+        <div className="toDoList">
+            <form>
+                <input type="text" placeholder="Enter To Do Item" />
+                <button onClick={handleButtonClick}className="addNew">Add To Do</button>
+            </form>
+        </div>
+    );
+};
+
+export default ToDoList;
